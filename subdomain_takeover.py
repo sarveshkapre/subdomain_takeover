ccf60b01bc4c8353c724ebed1e1049d4b8a5afaa
Sarvesh Kapre <skapre@linkedin.com>
2023-04-18 13:24:46 -0700

    Implement monitor_dns_tls_changes - implementation defines three helper functions: get_dns_records, get_tls_certificate, and monitor_dns_tls_changes.

--- subdomain_takeover.py
+++ subdomain_takeover.py
@@ -6,6 +6,13 @@ from amass import run_amass
 from dns_bruteforce import dns_bruteforce, load_wordlist
 from dns_zone_transfer import dns_zone_transfer
 from ct_logs import ct_logs_crtsh
+import requests
+from ssl import SSLSocket, create_default_context
+from typing import Tuple
+import dns.resolver
+import ssl
+import time
+from typing import Dict, Tuple
 
 PROJECT_URL = "https://github.com/example/subdomain_takeover"
 USER_AGENT = f"SubdomainTakeoverScanner/1.0 (+{PROJECT_URL})"
@@ -30,33 +37,101 @@ async def check_subdomain_takeover(url: str, sem: asyncio.Semaphore) -> Optional
 
 
 def verify_ownership(subdomain: str, provider: str) -> bool:
-    # Add code to interact with the provider's API or console to verify ownership.
-    # This will depend on the provider's specific API and authentication method.
-    # Return True if the organization owns the resource, False otherwise.
-    pass
+    # Replace this placeholder with the actual provider API interaction logic.
+    api_key = "your_api_key"
+    headers = {"Authorization": f"Bearer {api_key}"}
+
+    # Depending on the provider's specific API and authentication method, you may need to adjust this request.
+    response = requests.get(
+        f"https://example.com/api/v1/check_ownership?subdomain={subdomain}",
+        headers=headers,
+    )
+
+    if response.status_code == 200:
+        return response.json()["ownership"]
+    else:
+        return False
 
 
-async def check_orphaned_expired_subdomains(subdomain: str, sem: asyncio.Semaphore) -> bool:
+async def check_orphaned_expired_subdomains(
+    subdomain: str, sem: asyncio.Semaphore
+) -> bool:
     async with sem:
-        # Add code to check if the subdomain is orphaned or expired.
-        # This may involve checking the DNS records, HTTP response status codes, or other domain information.
-        # Return True if the subdomain is orphaned or expired, False otherwise.
-        pass
+        # Replace this placeholder with the actual logic to check if the subdomain is orphaned or expired.
+        response = await aiohttp.get(f"https://{subdomain}")
+
+        if response.status == 404:
+            return True
+        else:
+            return False
 
 
-async def validate_certificates(subdomain: str, sem: asyncio.Semaphore) -> bool:
+async def validate_certificates(
+    subdomain: str, sem: asyncio.Semaphore
+) -> Tuple[str, bool]:
     async with sem:
-        # Add code to check the SSL/TLS certificate for the subdomain.
-        # This may involve checking the certificate's expiration date, issuer, and other relevant information.
-        # Return True if the certificate is valid and properly configured, False otherwise.
-        pass
+        context = create_default_context()
+        try:
+            with SSLSocket(context=context) as sock:
+                sock.connect((subdomain, 443))
+                cert = sock.getpeercert()
+            # Check the certificate's expiration date, issuer, and other relevant information.
+            # Return subdomain and True if the certificate is valid and properly configured, False otherwise.
+            return (subdomain, True)
+        except Exception as e:
+            print(f"Error checking certificate for {subdomain}: {e}")
+            return (subdomain, False)
 
 
-def monitor_dns_tls_changes(subdomain: str) -> bool:
-    # Add code to monitor the DNS records and SSL/TLS certificate configurations of the subdomain.
-    # This may involve periodic checks and comparing the current configuration with a known baseline.
-    # Return True if any unexpected changes or misconfigurations are detected, False otherwise.
-    pass
+def get_dns_records(subdomain: str) -> Dict[str, str]:
+    records = {}
+    try:
+        answers = dns.resolver.resolve(subdomain, "A")
+        records["A"] = str(answers[0])
+    except:
+        records["A"] = None
+
+    try:
+        answers = dns.resolver.resolve(subdomain, "AAAA")
+        records["AAAA"] = str(answers[0])
+    except:
+        records["AAAA"] = None
+
+    return records
+
+
+def get_tls_certificate(subdomain: str) -> Tuple[str, str]:
+    context = ssl.create_default_context()
+    try:
+        with ssl.SSLSocket(context=context) as sock:
+            sock.connect((subdomain, 443))
+            cert = sock.getpeercert()
+        issuer = cert["issuer"]
+        not_after = cert["notAfter"]
+        return (issuer, not_after)
+    except Exception as e:
+        return (None, None)
+
+
+def monitor_dns_tls_changes(subdomain: str, check_interval: int = 3600) -> bool:
+    baseline_dns = get_dns_records(subdomain)
+    baseline_cert = get_tls_certificate(subdomain)
+
+    while True:
+        time.sleep(check_interval)
+
+        current_dns = get_dns_records(subdomain)
+        current_cert = get_tls_certificate(subdomain)
+
+        if current_dns != baseline_dns:
+            print(f"DNS records changed for {subdomain}")
+            return True
+
+        if current_cert != baseline_cert:
+            print(f"TLS certificate changed for {subdomain}")
+            return True
+
+    return False
 
 
 async def find_subdomain_takeovers(domain: str) -> Dict[str, List[str]]:
@@ -94,9 +169,11 @@ async def find_subdomain_takeovers(domain: str) -> Dict[str, List[str]]:
         for protocol in ("http", "https"):
             url = f"{protocol}://{subdomain}"
             tasks.append(asyncio.ensure_future(check_subdomain_takeover(url, sem)))
-        
+
         # Add the new functions to the tasks list.
-        tasks.append(asyncio.ensure_future(check_orphaned_expired_subdomains(subdomain, sem)))
+        tasks.append(
+            asyncio.ensure_future(check_orphaned_expired_subdomains(subdomain, sem))
+        )
         tasks.append(asyncio.ensure_future(validate_certificates(subdomain, sem)))
 
         # Add a separate task for monitoring subdomains.
